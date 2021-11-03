c87c77da4fd19c0781556e80ca653b061700fcc5
Lauro Costa <joaolauro94@gmail.com>
2021-11-03 19:49:29 -0300

    feat: created component separator

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {View} from 'react-native';
 import {ThemeProvider} from 'styled-components';
 import Button from './components/Button';
 import Icon from './components/Icon';
+import Separator from './components/Separator';
 import useAppearance from './hooks/useAppearence';
 import {Container} from './style';
 
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <Container>
         <View style={{flexDirection: 'row', justifyContent: 'center'}}>
           <Icon icon="folha1" activeColor="secondary" size={200}></Icon>
+          <Separator width={20}/>
           <Icon icon="folha2" size={200}></Icon>
         </View>
 
