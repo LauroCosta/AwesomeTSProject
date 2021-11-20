af0c3749696803bc3c1214ca7c90054a55ea6aa0
Lauro Costa <joaolauro94@gmail.com>
2021-11-20 20:37:45 -0300

    Stack navigation setup

--- src/App.tsx
+++ src/App.tsx
@@ -9,48 +9,15 @@ import Separator from '~/components/Separator';
 import useAppearance from '~/hooks/useAppearence';
 import {Container} from './style';
 import {NavigationContainer} from '@react-navigation/native';
+import Routers from './navigation';
 
 const App: React.FC = () => {
   const {theme} = useAppearance();
 
-  const refEmail = useRef<InputValueRef>({value: ''});
-  const refPassword = useRef<InputValueRef>({value: ''});
-
-  const handlePress = () => {
-    Alert.alert(
-      refEmail.current?.value || 'null',
-      refPassword.current?.value || 'null',
-    );
-  };
-
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <Container>
-          <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-            <Icon icon="folha2" size={200}></Icon>
-          </View>
-          <View>
-            <Input
-              ref={refEmail}
-              label="E-mail"
-              iconPosition="right"
-              placeholder="jonhdoe@gmail.com"
-              icon="folha2"
-            />
-            <Separator height={20} />
-            <Input
-              ref={refPassword}
-              secureTextEntry
-              label="Password"
-              placeholder="Sua senha"
-            />
-          </View>
-          <Button color="secondary" mode="contained" onPress={handlePress}>
-            Login
-          </Button>
-          <Separator height={20} />
-        </Container>
+        <Routers />
       </NavigationContainer>
     </ThemeProvider>
   );
---
+++ src/navigation/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Home from '~/screen/Home';
+import Profile from '~/screen/Profile';
+
+const Stack = createNativeStackNavigator();
+
+const Routers = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="profile" component={Profile}></Stack.Screen>
+      <Stack.Screen name="home" component={Home}></Stack.Screen>
+    </Stack.Navigator>
+  );
+};
+
+export default Routers;
