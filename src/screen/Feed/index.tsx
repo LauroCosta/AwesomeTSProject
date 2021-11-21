118ea872e32e38a1ae85567312bc73319989ee11
Lauro Costa <joaolauro94@gmail.com>
2021-11-20 23:41:18 -0300

    Navigate nested navigator

--- src/screen/Feed/index.tsx
+++ src/screen/Feed/index.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import {Button, View} from 'react-native';
 
 // import { Container } from './styles';
 
 const Feed: React.FC = ({navigation}) => {
-
   const handlePress = () => {
-
     navigation.navigate('stories');
-  }
-
-  return <View style={{flex: 1, backgroundColor: 'green'}}>
-    <Button title="Go to Stories" onPress={handlePress} />
-  </View>;
-}
+  };
+  const handlePressToPost = () => {
+    navigation.navigate('profile',{
+      screen: 'post'
+    });
+  };
+  return (
+    <View style={{flex: 1, backgroundColor: 'green'}}>
+      <Button title="Go to Stories" onPress={handlePress} />
+      <Button title="Go to Posts" onPress={handlePressToPost} />
+    </View>
+  );
+};
 
 export default Feed;
